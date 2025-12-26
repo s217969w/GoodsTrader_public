4308408728d52d120fe79195da224926afff2aaa
s217969w <136055948+s217969w@users.noreply.github.com>
2025-12-26 09:50:21 +0900

    色の変更

--- src/components/Menu/SideMenu.tsx
+++ src/components/Menu/SideMenu.tsx
@@ -16,12 +16,12 @@ export default function SideMenu() {
   ]
   const currentpath = useLocation().pathname;
   return (
-    <div>
+    <div style={{backgroundColor:"lightblue"}}>
       <Stack minW={"180px"} width={"10vw"} height={"100vh"}>
         <Link to={"/"} key={"/"}>
           <Box
-            background={"blue.100"}
-            _hover={{ bg: "blue.200" }}
+            background={"blue.400"}
+            _hover={{ bg: "blue.500" }}
           >
             <Center minH={"80px"} height={"10vh"} width={"10vw"} minW={"180px"}>
               <Text color={"black"} textStyle={"lg"}>Goods trader</Text>
@@ -33,9 +33,9 @@ export default function SideMenu() {
             <Box
               background={
                 currentpath === item.to ?
-                  "pink.200" : "blue.100"
+                  "pink.200" : "blue.400"
               }
-              _hover={{ bg: currentpath === item.to ? "pink.300" : "blue.200" }}>
+              _hover={{ bg: currentpath === item.to ? "pink.300" : "blue.500" }}>
               <Center minH={"50px"} height={"6vh"} width={"10vw"} minW={"180px"}>
                 <HStack width="100%" justifyContent="flex-start" pl={4}>
                   <Box width={"30px"}>
